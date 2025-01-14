d0e4e9bcfb1aeb7bc1a4a2d1b3e0dcb6d59f8d90
Cecep Budiman <c3budiman@gmail.com>
2025-01-14 17:21:25 +0700

    add wedding configuration and update homepage with dynamic title and background

---
+++ wedding-fe/src/config/index.ts
@@ -0,0 +1,9 @@
+const Config = {
+    brides_man: "Cecep Budiman Wahid",
+    brides_woman: "Winia Dwi Wahyuni",
+    main_images: "/images/bg.png",
+    main_background: "",
+    main_song: "/music/beautiful_in_white.mp3",
+}
+
+export default Config
